cbb257b50bbda41546457d4de955661883fa9da7
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-05-19 02:17:35 +0300

    update

--- resources/views/posts/partials/item.blade.php
+++ resources/views/posts/partials/item.blade.php
@@ -30,7 +30,7 @@
         <div class="component-like d-block text-right">
             <div class="component-like__count d-inline-block">{{ $post->like()->count() }}</div>
             @if (Auth::guest())
-                <a href="{{ route('login') }}" class="component-like__button d-inline-block like--noliked">
+                <a href="{{ route('login') }}" class="ajax-modal component-like__button d-inline-block like--noliked">
                     <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"
                          xmlns:svg="http://www.w3.org/2000/svg">
                         <path d="m9.93287,4.06894c0.79747,-1.91386 2.61926,-3.25113 4.73822,-3.25113c2.85439,0 4.91012,2.47246 5.16857,5.41908c0,0 0.1395,0.73145 -0.16756,2.04831c-0.4181,1.79342 -1.40092,3.38677 -2.72596,4.60279l-7.01327,6.3358l-6.89512,-6.3362c-1.32504,-1.21562 -2.30786,-2.80937 -2.72596,-4.60279c-0.30706,-1.31686 -0.16756,-2.04831 -0.16756,-2.04831c0.25845,-2.94662 2.31418,-5.41908 5.16857,-5.41908c2.11935,0 3.82258,1.33766 4.62006,3.25153l0.00001,0z"/>
